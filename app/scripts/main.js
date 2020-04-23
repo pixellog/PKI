56a35332479dc751a29aa5ff683e402ecccf4d60
leeokhee <pixellog@gmail.com>
2020-04-23 16:58:00 +0900

    최종수정

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,11 @@
-$(function () {
-  pageInit();
-  // 공인인증서 복사하기 팝업
-  modalCertCopy();
-  // 알림 팝업
-  // modalAlert();
-});
-
-function pageInit() {
-  $('[data-toggle="popover"]').popover();
-  btnDisabled();
-  selectCert();
-}
+// 공인인증서 복사하기 팝업
+modalCertCopy();
 
 // 공인인증서 복사하기 팝업
 function modalCertCopy() {
-  $('#cert-copy-modal').modal('show',function () {
-    $('#cert').show();
-    $('#cert-save-path').hide();
-  });
+  $('#cert-copy-modal').modal('show');
+  certExportInit();
+  selectDevice();
 }
 
 // 알림 팝업
@@ -25,26 +13,93 @@ function modalAlert() {
   $('#alert-modal').modal('show');
 }
 
-// 인증서 가져오기 확인
+// 인증서 내보내기 초기화
+function certExportInit() {
+  selectCertInit(true);
+  // 탭 활성화 초기화
+  $('.tab-content .tab-pane').removeClass('active');
+  $('.nav .nav-link').removeClass('active');
+  // 첫번째탭 활성화
+  $('.tab-content .tab-pane').eq(0).addClass('active');
+  $('.nav .nav-link').eq(0).addClass('active');
+
+  $('#cert').show();
+  $('#cert-export-select').show();
+  $('#cert-save-path').hide();
+}
+
+// 인증서 가져오기 > 확인 버튼
 function certImportOk() {
   $('#cert-export-select').hide();
   $('#cert-save-path').show();
 }
 
-// 인증서 선택
-function selectCert() {
-  $(document).on('click', '.table-hover tbody tr', function () {
-    $(this).addClass('bg-primary').siblings().removeClass('bg-primary');
-  })
-}
-// 인증서 선택 방지
+// 인증서 선택 비활성화
 function selectCertDisabled() {
-  $('.table').removeClass('table-hover').addClass('table-secondary');
-  $('#input-cert-number').show();
-  $('#input-cert-password').hide();
+  // 1. 유효성 검증
+  // 선택된 인증서 유무 체크하고 없으면 선택유도 얼럿 출력
+  const check_cert = !$('.table .bg-primary').length;
+  if (check_cert) {
+    alert('선택된 인증서가 없습니다. 인증서를 선택해 주세요.');
+    return;
+  }
+  // todo : 인증서 비밀번호 체크
+  const check_pw = '11';
+  const cert_pw = $('#cert-pw').val();
+  if (check_pw !== cert_pw) {
+    alert('인증서 비밀번호가 유효하지 않습니다.');
+    return;
+  }
+
+  // 2. 유효성 검증 후
+  selectCertInit(false);
 }
 
-// 저장매체 disabled 스타일링 처리
-function btnDisabled() {
+// 인증서 선택 초기화
+function selectCertInit(x) {
+  if (x) {
+    $('#tab-export').removeClass('disabled');
+    $('.btn-group-toggle .btn').removeClass('disabled');
+    $('.table').addClass('table-hover').removeClass('table-secondary');
+    $('#input-cert-password').show();
+    $('#input-cert-number').hide();
+  } else {
+    // 인증서 내보내기 탭 비활성화
+    $('#tab-export').addClass('disabled');
+    // 저장매체 선택 버튼 비활성화
+    $('.btn-group-toggle .btn').addClass('disabled');
+    // 인증서 선택 테이블 비활성화
+    $('.table').removeClass('table-hover').addClass('table-secondary');
+    // 인증 비밀번호 폼 숨기고
+    $('#input-cert-password').hide();
+    // 인증번호 폼 보이기
+    $('#input-cert-number').show();
+  }
+}
+
+// 저장매체
+function selectDevice() {
   $(':disabled').closest('.btn').addClass('disabled');
+
+  $(document).on('click', '[data-toggle="popover"]', function () {
+    $('[data-toggle="popover"]').popover('hide');
+    $(this).popover('show');
+  });
+
+  $(document).on('click', '[data-toggle="tab"]', function () {
+    $('[data-toggle="popover"]').popover('hide');
+  });
+
+  // 이동식 디스크 선택
+  $(document).on('click', '.popover a', function () {
+    const active = 'active';
+    $(this).addClass(active).siblings().removeClass(active);
+  });
+
+  // 인증서 리스트 선택
+  $(document).on('click', '.table-hover tbody tr', function () {
+    const active = 'bg-primary';
+    $(this).addClass(active).siblings().removeClass(active);
+  })
+
 }
